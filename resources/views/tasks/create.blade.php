85a4a563c78874532b2dc96740906bccd47d70a3
Noboru.kamita <noboru.kamita@gamil.com>
2018-10-05 06:15:24 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,18 +4,23 @@
 
 <!-- ここにページ毎のコンテンツを書く -->
 
+<div class="row">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
     <h1>タスク新規作成ページ</h1>
-
+    
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
+        <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-        
+        {!! Form::text('content',null,['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">        
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::submit('タスク追加') !!}
+        {!! Form::text('status',null,['class'=>'form-control']) !!}
+        </div>
+        {!! Form::submit('タスク追加', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
+    </div>
+</div>
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,11 +5,23 @@
 <!-- ここにページ毎のコンテンツを書く -->
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
-
-    <p>タスク内容 - {{ $task->content }}</p>
-    <p>ステータス - {{ $task->status }}</p>
     
-     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id]) !!}
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>    
+
+    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id],['class'=>'btn btn-default']) !!}
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
     
